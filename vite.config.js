3eb4b3ccc039a11b2669ecec5916a1b1c2656c62
inakizamores <64514642+inakizamores@users.noreply.github.com>
2024-12-09 06:29:19 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Add any other Vite configuration here
   define: {
     'import.meta.env.VITE_SPOTIFY_CLIENT_ID': JSON.stringify(process.env.SPOTIFY_CLIENT_ID),
     'import.meta.env.VITE_REDIRECT_URI': JSON.stringify(process.env.REDIRECT_URI),
